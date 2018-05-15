3fdd85f3f2ca6269b650af0114269cffc484e63a
mamedov <asdfds@asdfs.cosd>
2018-05-15 20:27:44 +0300

    session_first

--- 03_REGEXP/index.php
+++ 03_REGEXP/index.php
@@ -1,7 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mamedov
- * Date: 15.05.2018
- * Time: 18:46
+
+
+/***
+ * \n
+ * \t
+ * \s - space пробельный символ
+ * \S - НЕ пробельный символ
+ * \w - wordsymbol буква/цифра
+ * \W -
+ * \b - граница слова \w\s  \s\w
+ * \B
+ * \d - digit
+ * \D
+ *
+ * .
+ *
+ * ?
+ * *
+ * +
+ * {n}
+ * {n,m}
+ * {n,}
+ *
+ * [abcd]
+ * [a-z]
+ * [0-9]
+ * [a-zA-Z0-9]
+ * [^abc]
+ * [^abcdz]
+ *
+ * ^ - start of text
+ * $ - end of text
+ *
+ * ()
+ * (0|80|380|\+380)(\d{9})
+ *
  */
+$rexp = '/^([a-z0-9][a-z0-9\-\.\_]*)@[a-z0-9]+(\.[a-z]{2,10}){1,4}$/i';
+$email = "das-asdas-d@mail.com.ua";
+
+if(preg_match($rexp,$email)){
+    echo "valid";
+}else{
+    echo "invalid";
+}
+
+echo preg_replace(
+    "/(0|80|380|\+380)(\d{9})/",
+    '0$2',
+    "dfdagsdfgm agasdf g 0930001122 dsagsdf cvzdf +380684445566");
+
